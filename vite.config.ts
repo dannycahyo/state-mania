1e2129c6ffca054b6de74ae78b5f59da47a657db
dannycahyo <dannydwicahyono@gmail.com>
2023-09-09 10:30:46 +0700

    chore: Configure the ts import paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@stores": path.resolve(__dirname, "./src/stores"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@containers": path.resolve(__dirname, "./src/containers"),
+      "@types": path.resolve(__dirname, "./src/types"),
+    },
+  },
+});
